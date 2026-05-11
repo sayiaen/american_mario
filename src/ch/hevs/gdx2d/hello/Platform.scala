6f6ffedde533e88f0733148aac97b8089a2a1585
Eren Sayinta <erensayinta@gmail.com>
2026-05-11 13:59:19 +0200

    collision added

--- src/ch/hevs/gdx2d/hello/Platform.scala
+++ src/ch/hevs/gdx2d/hello/Platform.scala
@@ -10,6 +10,6 @@ class Platform(var x: Float, var y: Float, var width: Float, var height: Float) 
 
   override def draw(g: GdxGraphics): Unit = {
     g.setColor(Color.BLACK)
-    g.drawFilledRectangle(x, y, width, height, 0)
+    g.drawFilledRectangle(x + width/2, y + height/2, width, height, 0)
   }
 }
--- src/ch/hevs/gdx2d/hello/Player.scala
+++ src/ch/hevs/gdx2d/hello/Player.scala
@@ -16,22 +16,42 @@ class Player(var x: Float, var y:Float) extends Entity {
   var onGround = false
 
   override def update(dt: Float, platforms: List[Platform]): Unit = {
-    vy += gravity * dt
 
+    vy += gravity * dt
     x += vx *dt
     y += vy * dt
 
-    if(y < 100){
-      y = 100
-      vy = 0
-      onGround = true
+    platforms.foreach { P =>
+      val marioLeft = x
+      val marioRiht= x + width
+      val marioBottom = y
+      val marioTop = y + height
+
+      val platformLeft = P.x
+      val platformRight = P.x + P.width
+      val platformBottom = P.y
+      val platformTop = P.y + P.height
+
+      if(marioRiht > platformLeft &&
+         marioLeft < platformRight &&
+         marioBottom < platformTop &&
+         marioTop > platformBottom) {
+
+        if(vy < 0) {
+          y = platformTop
+          vy = 0
+          onGround = true
+        }
+      }
     }
+
+
 
   }
 
   override def draw(g: GdxGraphics): Unit = {
     g.setColor(Color.BLUE)
-    g.drawFilledRectangle(x,y,width,height,0)
+    g.drawFilledRectangle(x + width/2,y + height/2 ,width,height,0)
 
   }
 }
